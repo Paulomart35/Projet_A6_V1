a967d1b615e256c1c17bcc9f9f83c5d16dcedb8b
Paul Martin <paul.martin@funbooker.com>
2024-04-30 10:30:55 +0200

    Ajout historique client

--- Projet_A6_V1/Client.cs
+++ Projet_A6_V1/Client.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Projet_A6_V1
 {
     internal class Client : Personne
     {
+        //peut-être mettre ça en List car un client plusieurs module
         public int num_commande;
 
         public Client(int num_ss, string nom, string prenom, DateTime date_naissance, string adresse, string mail, string telephone, int num_commande) 
@@ -43,7 +46,30 @@ namespace Projet_A6_V1
             Console.Write("Numéro de commande : ");
             int num_commande = Convert.ToInt32(Console.ReadLine());
 
+            Ecrire_client_excel(num_ss, nom, prenom, date_naissance, adresse, mail, telephone, num_commande);
+
             return new Client(num_ss, nom, prenom, date_naissance, adresse, mail, telephone, num_commande);
+
+        }
+
+        //Historique pour le module client
+        public static void Ecrire_client_excel(int num_ss, string nom, string prenom, DateTime date_naissance, string adresse, string mail, string telephone, int num_commande)
+        {
+            string path = "Client_Transconnect.csv";
+            try
+            {
+                string text = (num_ss + "," + nom + "," + prenom + "," + date_naissance + "," + adresse + "," + mail + "," + telephone + "," + num_commande);
+                using (StreamWriter writer = new StreamWriter(path, true))
+                {
+                    writer.WriteLine(text);
+                }
+                Console.WriteLine("ok");
+            }
+            catch(Exception ex) 
+            {
+                throw new ApplicationException("Erreur dans le programme :", ex);
+            }
+
         }
     }
 }
