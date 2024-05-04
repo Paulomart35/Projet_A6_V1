86a3ce4aee9eb07d340db1477115bf53055959eb
JeanJeanSan <jean-baptiste.mathevon@edu.devinci.fr>
2024-05-04 20:22:26 +0200

    ajout class noeud

---
+++ Projet_A6_V1/Noeud.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_A6_V1
+{
+    internal class Noeud
+    {
+        public int Valeur { get; set; }
+        public Noeud Fils { get; set; }
+        public Noeud Frere { get; set; }
+
+        public Noeud(int valeur)
+        {
+            Valeur = valeur;
+            Fils = null;
+        }
+
+        public void AjouterFils(Noeud fils)
+        {
+            Fils = fils;
+        }
+
+        public void AjouterFrere(Noeud frere)
+        {
+            Frere = frere;
+        }
+    }
+}
